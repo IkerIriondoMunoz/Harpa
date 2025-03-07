f93986476ff9603480ff6ec557de04ad803f0cdd
Alvaro2rh <alvaro.ramirez@live.u-tad.com>
2025-03-07 18:03:47 +0100

    Menus

---
+++ Harpa/Assets/Scripts/MenuOpciones.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MenuOpciones : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void PantallaCompleta(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+
+    public void CambiarVolumen(float volumen)
+    {
+        audioMixer.SetFloat("Volumen", volumen);
+    }
+}

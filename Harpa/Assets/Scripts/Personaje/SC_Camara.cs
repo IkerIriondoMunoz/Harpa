a6e463548b61f2c781022b102b9c9193b92717b0
Alvaro2rh <alvaro.ramirez@live.u-tad.com>
2025-03-09 16:42:53 +0100

    Camara Script

--- Harpa/Assets/Scripts/Personaje/SC_Camara.cs
+++ Harpa/Assets/Scripts/Personaje/SC_Camara.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class SC_Camara : MonoBehaviour
 {
-    
+    public Transform target;
+    public Vector3 offset;
+    public float rotationSpeed = 3f;
+
+    private float currentX = 0f;
+    private float currentY = 0f;
+    public float minY = -20f, maxY = 60f;
+
+    void Update()
+    {
+        currentX += Input.GetAxis("Mouse X") * rotationSpeed;
+        currentY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        currentY = Mathf.Clamp(currentY, minY, maxY);
+    }
+
+    void LateUpdate()
+    {
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = target.position + rotation * offset;
+        transform.LookAt(target.position);
+    }
 }
--- Harpa/Assets/Scripts/Personaje/SC_Movimiento.cs
+++ Harpa/Assets/Scripts/Personaje/SC_Movimiento.cs
@@ -6,7 +6,7 @@ public class SC_Movimiento : MonoBehaviour
     [SerializeField] private float runSpeed = 12f;
     private float currentSpeed;
 
-    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float jumpForce = 2f;
     [SerializeField] private float gravity = -9.81f;
 
     public CharacterController characterController;

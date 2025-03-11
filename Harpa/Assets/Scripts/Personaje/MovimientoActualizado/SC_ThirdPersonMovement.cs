7e95ec6b491f3b13a4cfa2c1365e7879ba1c5ab3
Iker <Ikeririondo27@gmail.com>
2025-03-11 20:23:38 +0100

    Camara2Modos

--- Harpa/Assets/Scripts/Personaje/MovimientoActualizado/SC_ThirdPersonMovement.cs
+++ Harpa/Assets/Scripts/Personaje/MovimientoActualizado/SC_ThirdPersonMovement.cs
@@ -6,25 +6,59 @@ public class SC_ThirdPersonMovement : MonoBehaviour
     public CharacterController cc;
     public Transform Camara;
 
+
     public float speed = 6f;
 
+    private bool cameraLock = false;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direccion = new Vector3 (horizontal, 0f, vertical).normalized;
+        MovementManager();
+        CameraLockManager();
 
-        if (direccion.magnitude > 0.1f )
+        if (Input.GetKeyDown(KeyCode.Q) && cameraLock == false)
         {
-            float targetAngle = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + Camara.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            cameraLock = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && cameraLock == true)
+        {
+            cameraLock = false;
+        }
+        Debug.Log(cameraLock);
+    }
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle,0f) * Vector3.forward;
-            cc.Move(moveDirection.normalized * speed * Time.deltaTime);
+    void CameraLockManager()
+    {
+        if (cameraLock == true)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direccion = transform.forward * vertical + transform.right * horizontal;
+
+            transform.rotation = Quaternion.Euler(0f,Camara.eulerAngles.y,0f);
+            
+            cc.Move(direccion * speed * Time.deltaTime);
+        }
+    }
+    void MovementManager()
+    {
+        if (cameraLock == false)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direccion = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direccion.magnitude > 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + Camara.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                cc.Move(moveDirection.normalized * speed * Time.deltaTime);
+            }
         }
     }
 }

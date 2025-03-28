b93dc4f9d29e82def314b9cd38d9b687e0f9e8ef
Alvaro2rh <alvaro.ramirez@live.u-tad.com>
2025-03-28 16:52:38 +0100

    text

--- Harpa/Assets/Scripts/IA y Textos/Texts.cs
+++ Harpa/Assets/Scripts/IA y Textos/Texts.cs
@@ -1,27 +1,41 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Texts : MonoBehaviour
 {
     public Canvas canvasText;
+    public Canvas canvasInput;
     public Text _text;
     public string _message;
+    public GameObject _player;
 
     void Start()
     {
         canvasText.gameObject.SetActive(false);
     }
 
-    void Update()
-    {
+    private void OnTriggerEnter(Collider _collider)
+    {  
+        canvasInput.gameObject.SetActive(true);
        if (Input.GetKey(KeyCode.E))
         {
+            canvasInput.gameObject.SetActive(false);
             canvasText.gameObject.SetActive(true);
+            _player.SetActive(false);
             _text.text = _message;
         }
        if (Input.GetKey(KeyCode.E))
         {
+            _player.SetActive(false);
+            canvasInput.gameObject.SetActive(true);
             canvasText.gameObject.SetActive(false);
         }
     }
+
+    private void OnTriggerExit(Collider _collider)
+    {
+        canvasText.gameObject.SetActive(false);
+    }
+
 }

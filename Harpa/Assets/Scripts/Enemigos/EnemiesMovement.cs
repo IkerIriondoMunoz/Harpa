647001343bfacd468a6cdc3f5ae0f6046667d4f8
Alvaro2rh <alvaro.ramirez@live.u-tad.com>
2025-05-21 10:10:55 +0200

    Cambios y cosillas

---
+++ Harpa/Assets/Scripts/Enemigos/EnemiesMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemiesMovement : MonoBehaviour
+{
+    public NavMeshAgent _agent;
+    public GameObject[] _routePoints;
+    public float _speed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
